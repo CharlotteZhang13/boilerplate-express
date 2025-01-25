fbcf9ed5d4cd4afb2b38775f0a397ebc2754ae5c
CharlotteZhang13 <zhangxiyue0128@outlook.com>
2025-01-25 16:17:44 -0500

    versions

--- myApp.js
+++ myApp.js
@@ -1,9 +1,21 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
+
+absolutePath = __dirname + '/views/index.html'
 
 app.listen(3000, ()=>{
-    console.log("Hello World")
-})
+    console.log("Server is running on port 3000")
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(absolutePath)
+});
+app.get('/json', (req, res) => {
+    res.json({"message": "hello json"})
+});
+
+app.use('/public', express.static(__dirname + '/public'))
 
 
 
